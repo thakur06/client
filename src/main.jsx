50f55cdfd638ea38dbba540eedc14250b7964ac4
ABHISHEK RAJPUT <79101220+thakur06@users.noreply.github.com>
2025-04-17 16:44:32 +0530

    added changes for pwa

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { registerSW } from 'virtual:pwa-register';
 import { LiefContext } from "./context/AppData.jsx";
+registerSW();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LiefContext>
--- vite.config.js
+++ vite.config.js
@@ -6,51 +6,33 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-//     VitePWA({
-//       registerType: 'autoUpdate', // Automatically updates the service worker
-//       devOptions: {
-//         enabled: true, // Enable PWA features during development
-//       },
-//       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.png'],
-//       manifest: {
-//         name: 'Careworker Clock-In System',
-//         short_name: 'CareClock',
-//         description: 'A system for careworkers to clock in and out with location-based restrictions',
-//         theme_color: '#000000',
-//         background_color: '#ffffff',
-//         display: 'standalone',
-//         scope: '/',
-//         start_url: '/',
-//         icons: [
-//           {
-//             src: '/pwa-192x192.png',
-//             sizes: '192x192',
-//             type: 'image/png',
-//           },
-//           {
-//             src: '/pwa-512x512.png',
-//             sizes: '512x512',
-//             type: 'image/png',
-//           },
-//         ],
-//       },
-//       workbox: {
-//         // Optional: Customize caching strategies
-//         globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
-//         runtimeCaching: [
-//           {
-//             urlPattern: /^https?.*/,
-//             handler: 'NetworkFirst',
-//             options: {
-//               cacheName: 'offlineCache',
-//               expiration: {
-//                 maxEntries: 200,
-//               },
-//             },
-//           },
-//         ],
-//       },
-  
-// }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-icon-180.png'],
+      manifest: {
+        name: 'My Vite React App',
+        short_name: 'ReactApp',
+        description: 'A Vite-powered React PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
 })
